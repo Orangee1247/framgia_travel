568502aed04c23b6cada60261522bf4f46b193a9
Pho Duc Dat <phoducdat@gmail.com>
2017-08-02 11:12:26 +0700

    admin
    abc
    
    abc
    
    Login + Alter Table
    
    User Profile
    
    User profile
    
    admin

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function () {
+    return view('admin.admin_template');
+});
+Route::get('test', 'AdminController@index');
+
+Route::get('/user/profile/{id}', 'PagesController@showProfile')->name('user.profile');
+Route::post('/user/profile/{id}', ['as' => 'user.update', 'uses' => 'UserController@updateProfile']);
