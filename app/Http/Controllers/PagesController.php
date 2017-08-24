28c6e33031549dff64a2c2f06ad460b566ac413b
Pho Duc Dat <phoducdat@gmail.com>
2017-08-24 12:36:52 +0700

    request

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Plan;
+use App\Models\PlanLocation;
 use App\Models\Province;
 use App\Models\ProvinceGallery;
 use App\Models\Service;
+use App\Models\ServiceGallery;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PagesController extends Controller
 {
@@ -20,9 +26,26 @@ class PagesController extends Controller
     {
         $provinces = Province::where('name', '=', $request->name)->first();
 
-        $hotels = Service::where([['category_id', '=', 1], ['province_id', '=', $provinces->id],])->get();
+        $type = $request->id;
 
-        return view('pages.service.hotels.list', compact('hotels', 'provinces'));
+        $hotels = Service::where('category_id', '=', $type)
+            ->where('province_id', '=', $provinces->id)
+            ->get();
+
+
+        foreach ($hotels as $hotel) {
+            $hotel->rate_average = Comment::where('service_id', '=', $hotel->id)
+                ->where('rate_point', '>', '0')
+                ->avg('rate_point');
+
+            $hotel->comment = Comment::where('service_id', '=', $hotel->id)
+                ->get()
+                ->count();
+
+            $hotel->save();
+        }
+
+        return view('pages.service.hotels.list', compact('hotels', 'provinces', 'type'));
     }
 
     public function hotels(Request $request)
@@ -31,6 +54,37 @@ class PagesController extends Controller
         $provinces = Province::where('name', 'like', "%$key%")->take(30)->paginate(5);
 
         return view('pages.service.hotels.search', compact('provinces', 'key'));
+    }
+
+    public function hotelPF(Request $request)
+    {
+        $type = $request->id;
+
+        $hotel = Service::where('name', '=', $request->name1)->first();
+
+        $images = ServiceGallery::where('service_id', '=', $hotel->id)->get();
+
+        $comments = Comment::where('service_id', '=', $hotel->id)->orderBy('id', 'DESC')->get();
+
+        $hotel->rate_average = Comment::where('service_id', '=', $hotel->id)
+            ->where('rate_point', '>', '0')
+            ->avg('rate_point');
+
+        $hotel->comment = Comment::where('service_id', '=', $hotel->id)
+            ->get()
+            ->count();
+
+        $hotel->save();
+
+        $users = User::join('comments', 'users.id', '=', 'comments.user_id')
+            ->where('comments.service_id', '=', $hotel->id)
+            ->select('user_id', 'profile_pic', 'name')
+            ->distinct()
+            ->get();
+
+        $province = $request->name;
+
+        return view('pages.service.hotels.profile', compact('hotel', 'images', 'province', 'comments', 'users', 'type'));
     }
 
     public function provincePF(Request $request)
@@ -51,11 +105,114 @@ class PagesController extends Controller
     {
         $provinces = Province::all();
 
-        return view('pages.action.request.request', compact('provinces'));
+        return view('pages.action.request.add', compact('provinces'));
     }
 
-    public function requestPost()
+    public function requestPost(Request $request)
     {
-        return view('pages.action.request.request');
+        $choices = $request->proChoice;
+        $plan = new Plan();
+        $plan->fill($request->all());
+        $plan->user_id = Auth::user()->id;
+        $plan->save();
+        foreach ($choices as $choice) {
+            $pl = new PlanLocation();
+            $pl->province_id = $choice;
+            $pl->plan_id = $plan->id;
+            $pl->save();
+        }
+
+        return redirect(route('user.profile', Auth::user()->id));
+    }
+
+    public function requestEditGet(Request $request)
+    {
+        $plann = Plan::with('plan_location')->find($request->id);
+        $choices = $plann->plan_location;
+        $provinces = Province::all();
+//        $choices = DB::table('plans')->join('plan_locations', 'plans.id', '=', 'plan_locations.plan_id')
+//            ->join('provinces', 'provinces.id', '=', 'plan_locations.province_id')->where('plans.id', '=', $request->id)
+//            ->get();
+
+        return view('pages.action.request.edit', compact('plann', 'provinces', 'choices'));
+    }
+
+    public function requestEditPost(Request $request)
+    {
+        $plann = Plan::find($request->id);
+        $plann->update($request->all());
+        $plann->save();
+        $choices = is_array($request->proChoice) ? $request->proChoice : [];
+        $deletes = PlanLocation::where('plan_id', '=', $request->id)->get();
+        foreach ($deletes as $delete) {
+            $delete->delete();
+        }
+        foreach ($choices as $choice) {
+            $pl = new PlanLocation();
+            $pl->province_id = $choice;
+            $pl->plan_id = $request->id;
+            $pl->save();
+        }
+
+        return redirect(route('user.profile', Auth::user()->id));
+    }
+
+    public function requestDelete(Request $request)
+    {
+        $delete = Plan::find($request->id);
+        $delete->delete();
+
+        return redirect(route('user.profile', Auth::user()->id));
+    }
+
+    public function comment(Request $request)
+    {
+        $type = $request->id1;
+
+        $hotel = Service::where('name', '=', $request->name1)->first();
+
+        $user = Auth::user();
+
+        $comment = new Comment();
+        $comment->user_id = $user->id;
+        $comment->service_id = $hotel->id;
+        $comment->title = $request->title;
+        $comment->content = $request->content;
+        $comment->rate_point = $request->rate_point;
+        $comment->save();
+
+        switch ($type) {
+            case 1:
+                return redirect(route('hotelPF', [$request->name, $type, $request->name1]));
+                break;
+
+            case 2:
+                return redirect(route('restaurantPF', [$request->name, $type, $request->name1]));
+                break;
+
+            case 3:
+                return redirect(route('activityPF', [$request->name, $type, $request->name1]));
+                break;
+        }
+    }
+
+    public function editComment(Request $request)
+    {
+        $type = $request->id1;
+        $comment = Comment::find($request->id);
+        $comment->title = $request->title;
+        $comment->content = $request->content;
+        $comment->save();
+
+        return redirect(route('hotelPF', [$request->name, $type, $request->name1]));
+    }
+
+    public function deleteComment(Request $request)
+    {
+        $type = $request->id1;
+        Comment::destroy($request->id);
+
+        return redirect(route('hotelPF', [$request->name, $type, $request->name1]));
     }
 }
+
--- database/migrations/2017_08_22_012314_create_modify_table_v5.php
+++ database/migrations/2017_08_22_012314_create_modify_table_v5.php
@@ -12,8 +12,42 @@ class CreateModifyTableV5 extends Migration
      */
     public function up()
     {
+        Schema::table('services', function ($table) {
+            $table->text('description')->change();
+            $table->float('expected_price')->default(0)->change();
+            $table->integer('comment')->nullable()->default(0);
+            $table->string('phone')->nullable()->default(null);
+            $table->float('rate_average')->nullable()->default(null)->change();
+        });
+
         Schema::table('provinces', function ($table) {
-            $table->string('description', 1000)->change();
+            $table->text('description')->change();
+        });
+
+        Schema::table('plans', function ($table) {
+            $table->dropColumn('time');
+            $table->string('time_from')->nullable()->default(null);
+            $table->string('time_to')->nullable()->default(null);
+        });
+
+        Schema::table('service_gallerys', function ($table) {
+            $table->dropColumn('province_name');
+            $table->dropColumn('pic_name');
+        });
+
+        Schema::table('comments', function ($table) {
+            $table->text('content')->nullable()->default(null)->change();
+            $table->text('title')->nullable()->default(null);
+            $table->float('rate_point')->nullable()->default(0)->change();
+        });
+
+        Schema::table('comments', function ($table) {
+            $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->change();
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->change();
+        });
+
+        Schema::table('users', function ($table) {
+            $table->string('profile_pic')->default('profile_pic.png')->change();
         });
     }
 
