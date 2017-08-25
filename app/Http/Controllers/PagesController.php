1241637d3bbdf67e6e21c3b52e12feada28c5a44
Pho Duc Dat <phoducdat@gmail.com>
2017-08-25 14:29:26 +0700

    request
    basic request

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Plan;
 use App\Models\PlanLocation;
 use App\Models\Province;
 use App\Models\ProvinceGallery;
 use App\Models\Service;
+use App\Models\ServiceGallery;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,9 +26,26 @@ class PagesController extends Controller
     {
         $provinces = Province::where('name', '=', $request->name)->first();
 
-        $hotels = Service::where([['category_id', '=', 1], ['province_id', '=', $provinces->id],])->get();
+        $type = $request->id;
 
-        return view('pages.service.hotels.list', compact('hotels', 'provinces'));
+        $hotels = Service::where('category_id', '=', $type)
+            ->where('province_id', '=', $provinces->id)
+            ->get();
+
+
+        foreach ($hotels as $hotel) {
+            $hotel->rate_average = Comment::where('service_id', '=', $hotel->id)
+                ->where('rate_point', '>', '0')
+                ->avg('rate_point');
+
+            $hotel->comment = Comment::where('service_id', '=', $hotel->id)
+                ->get()
+                ->count();
+
+            $hotel->save();
+        }
+
+        return view('pages.service.hotels.list', compact('hotels', 'provinces', 'type'));
     }
 
     public function hotels(Request $request)
@@ -34,6 +54,37 @@ class PagesController extends Controller
         $provinces = Province::where('name', 'like', "%$key%")->take(30)->paginate(5);
 
         return view('pages.service.hotels.search', compact('provinces', 'key'));
+    }
+
+    public function hotelPF(Request $request)
+    {
+        $type = $request->id;
+
+        $hotel = Service::where('name', '=', $request->name1)->first();
+
+        $images = ServiceGallery::where('service_id', '=', $hotel->id)->get();
+
+        $comments = Comment::where('service_id', '=', $hotel->id)->orderBy('id', 'DESC')->get();
+
+        $hotel->rate_average = Comment::where('service_id', '=', $hotel->id)
+            ->where('rate_point', '>', '0')
+            ->avg('rate_point');
+
+        $hotel->comment = Comment::where('service_id', '=', $hotel->id)
+            ->get()
+            ->count();
+
+        $hotel->save();
+
+        $users = User::join('comments', 'users.id', '=', 'comments.user_id')
+            ->where('comments.service_id', '=', $hotel->id)
+            ->select('user_id', 'profile_pic', 'name')
+            ->distinct()
+            ->get();
+
+        $province = $request->name;
+
+        return view('pages.service.hotels.profile', compact('hotel', 'images', 'province', 'comments', 'users', 'type'));
     }
 
     public function provincePF(Request $request)
@@ -105,4 +156,63 @@ class PagesController extends Controller
 
         return redirect(route('user.profile', Auth::user()->id));
     }
+
+    public function requestDelete(Request $request)
+    {
+        $delete = Plan::find($request->id);
+        $delete->delete();
+
+        return redirect(route('user.profile', Auth::user()->id));
+    }
+
+    public function comment(Request $request)
+    {
+        $type = $request->id1;
+
+        $hotel = Service::where('name', '=', $request->name1)->first();
+
+        $user = Auth::user();
+
+        $comment = new Comment();
+        $comment->user_id = $user->id;
+        $comment->service_id = $hotel->id;
+        $comment->title = $request->title;
+        $comment->content = $request->content;
+        $comment->rate_point = $request->rate_point;
+        $comment->save();
+
+        switch ($type) {
+            case 1:
+                return redirect(route('hotelPF', [$request->name, $type, $request->name1]));
+                break;
+
+            case 2:
+                return redirect(route('restaurantPF', [$request->name, $type, $request->name1]));
+                break;
+
+            case 3:
+                return redirect(route('activityPF', [$request->name, $type, $request->name1]));
+                break;
+        }
+    }
+
+    public function editComment(Request $request)
+    {
+        $type = $request->id1;
+        $comment = Comment::find($request->id);
+        $comment->title = $request->title;
+        $comment->content = $request->content;
+        $comment->save();
+
+        return redirect(route('hotelPF', [$request->name, $type, $request->name1]));
+    }
+
+    public function deleteComment(Request $request)
+    {
+        $type = $request->id1;
+        Comment::destroy($request->id);
+
+        return redirect(route('hotelPF', [$request->name, $type, $request->name1]));
+    }
 }
+
